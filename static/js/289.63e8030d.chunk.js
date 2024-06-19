bbadea8e048e93bf2081c8d93e5811bb36a317b3
Paul Mattukat <pmattukat>
2024-06-19 08:39:56 +0200

    Updates

---
+++ static/js/289.63e8030d.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_dart=self.webpackChunkreact_dart||[]).push([[289],{289:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=289.63e8030d.chunk.js.map
